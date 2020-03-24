b435cb3d0e2b260819293db00352ffaaf92d7a6e
Eric <guillaumeric.progress@gmail.com>
2020-03-24 22:20:25 +0100

    config can now be override by variable

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 RAW_CURLED_IMAGES_DIR = "/tmp/AnimeFaceGAN/RawCurledImages"
 RAW_CURLED_IMAGES_GLOB = "{}/**/*.png".format(RAW_CURLED_IMAGES_DIR)
-DATA_DIR = "/Users/eric/dev/data/AnimeFaceGAN"
+DATA_DIR = os.environ["ANIME_FACE_GAN_DATA_DIR"] if "ANIME_FACE_GAN_DATA_DIR" in os.environ else "/Users/eric/dev/data/AnimeFaceGAN"
 DATA_GLOB = "{}/*.png".format(DATA_DIR)
 GENERATED_IMAGES_DIR = "./generated"
